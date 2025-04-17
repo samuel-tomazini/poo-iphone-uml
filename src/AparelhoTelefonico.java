8b1e77ba624dc95738e609920b00ba86f8ed52a5
Samuel Tomazini <samueltomazini@hotmail.com>
2025-04-16 21:42:51 -0300

    poo-iphone-desafio DIO

---
+++ src/AparelhoTelefonico.java
@@ -0,0 +1,11 @@
+public interface AparelhoTelefonico {
+    void ligar(String numero);
+
+    void atender();
+
+    void encerrarChamada();
+
+    void desligar();
+
+    void iniciarCorreioVoz();
+}
